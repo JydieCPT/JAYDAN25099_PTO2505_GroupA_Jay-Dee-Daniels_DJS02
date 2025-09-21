a2e25ea74d992a2109c430936891a33f46c87ce2
Jay-Dee Daniels <jydiecpt@gmail.com>
2025-09-21 14:46:42 +0200

    Update createModal.js file links.

--- src/createModal.js
+++ src/createModal.js
@@ -1,5 +1,5 @@
-import { GenreService } from "../utils/GenreService.js";
-import { DateUtils } from "../utils/DateUtils.js";
+import { GenreService } from "../src/genreService.js";
+import { DateUtils } from "../src/dateUtils.js";
 import { seasons } from "../data.js";
 
 /**
